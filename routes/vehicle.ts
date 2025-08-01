a61ae00fd5a931839026ea16b8e2029dabc6d895
Aadithya-J <jlaadithya@gmail.com>
2025-08-01 15:42:51 +0530

    setup create and get api for vehicle

---
+++ routes/vehicle.ts
@@ -0,0 +1,39 @@
+import { Router, Request, Response } from 'express';
+import prisma from '../utils/prismaClient'
+
+const router = Router();
+
+router.post("/create", async (req: Request,res: Response) => {
+    const {vin,manufacturer,model,fleetID,registrationStatus} = req.body;
+    // console.log(vin);
+    try {
+        const vehicle = await prisma.vehicle.create({
+            data: {
+                vin,
+                manufacturer,
+                model,
+                fleetID,
+                registrationStatus
+            }
+        })
+        res.json(vehicle);
+    } catch (error){
+        console.log(error);
+        res.json(error);
+    }
+});
+
+router.get("/getVehicle", async (req: Request,res: Response) => {
+    const vin : number = Number(req.query.vin);
+    try {
+        const vehicle = await prisma.vehicle.findUnique({
+            where: {vin}
+        })
+        res.json(vehicle);
+    } catch (error){
+        console.log(error);
+        res.json(error);
+    }
+})
+
+export default router;
