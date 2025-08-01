75042cedcb4d482be9ec0ab06b271c7e307d2f65
Aadithya-J <jlaadithya@gmail.com>
2025-08-01 15:34:28 +0530

    setup vehicle schema and routes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
+import vehicleRouter from '../routes/vehicle'
+
+
+app.use(express.json());
+
+
+app.use('/vehicle',vehicleRouter);
+
+
 app.get('/', (req: Request, res: Response) => {
   res.send("ok")
 });
